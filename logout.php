781acbca7df9f1467c81eebabb5db8cd23221b26
Suman Sahoo <sumansahoo3946@gmail.com>
2026-05-10 13:57:47 +0530

    UPDATED

--- logout.php
+++ logout.php
@@ -1,6 +1,17 @@
 <?php
+
 session_start();
+
+/* DESTROY SESSION */
+
+session_unset();
+
 session_destroy();
-header("Location: login.html");
+
+/* REDIRECT TO LOGIN PAGE */
+
+header("Location: http://localhost/event/login.html");
+
 exit();
+
 ?>
